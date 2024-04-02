dad8872bbb72db78bcc0ba24f7158c688fb5a4af
TakumiUemoto <ue0117tk@gmail.com>
2024-04-02 23:42:42 +0900

    refactor: 画像差し替え & style

--- src/app/(pages)/about/templates/staff.tsx
+++ src/app/(pages)/about/templates/staff.tsx
@@ -1,4 +1,4 @@
-import { Flex, VStack, Divider, Box, Image, Text } from '@chakra-ui/react';
+import { Flex, Divider, Box, Image, Text } from '@chakra-ui/react';
 
 export const Staff: React.FC = () => {
   return (
@@ -8,120 +8,133 @@ export const Staff: React.FC = () => {
       </Text>
       <Box mb='4rem'>
         <Flex
-          justify='space-evenly'
+          justify='space-between'
           align='flex-start'
-          flexDir={{ base: 'column', md: 'row' }}
+          flexDir={{ base: 'column', lg: 'row' }}
         >
-          <VStack width='full'>
+          <Flex
+            justify='center'
+            align='center'
+            flexDir='column'
+            w={{ base: 'full', lg: '30%' }}
+          >
             <Image
-              src='about/staff_hiroki.png'
-              borderRadius='full'
-              boxSize='200px'
+              src='about/staff_hiroki.jpg'
+              borderRadius='50%'
+              boxSize='300px'
               alt='staff'
             />
             <Text fontSize='2xl' fontWeight='bold'>
               ヒロキ
             </Text>
-          </VStack>
+          </Flex>
 
-          <Text
-            fontSize={{ base: 'lg', md: 'xl', lg: '2xl' }}
-            wordBreak='keep-all'
-            overflowWrap='anywhere'
-            textAlign='left'
-            px='0.5rem'
-          >
-            水中カメラマンのヒロキです！
-            <br />
-            いつも一眼レフで写真を撮っています！
-            <br />
-            <br />
-            広島県広島市出身の24歳。
-            <br />
-            広島の高専を卒業後、東京で電気メーカーのTV設計の仕事に3年弱従事していました。
-            2023年に退職をして趣味で始めた水中カメラを極める為に
-            宮古島への移住を決意しました。
-            <br />
-            海はもともと好きで高校２年生でスキューバダイビングのライセンスを取得するほどです。
-            <br />
-            僕たちのツアーで皆様の宮古島での素敵な思い出を残すお手伝いをさせてください。
-            <br />
-            全力でガイドをしていきます！
-            <br />
-            <br />
-            <span style={{ textDecoration: 'underline', fontWeight: 'bold' }}>
-              資格
-            </span>
-            <br />
-            ・NAUI スキンダイビングインストラクター
-            <br />
-            ・NAUI First Aider
-            <br />
-            ・NAUISkindiving Rescue Specialty Diver
-            <br />
-            ・フリーダイビング AIDA2
-            <br />
-            ・PADI ADVANCED OPEN WATER DIVER
-          </Text>
+          <Box w={{ base: 'full', lg: '70%' }}>
+            <Text
+              fontSize={{ base: 'lg', md: 'xl', lg: '2xl' }}
+              wordBreak='keep-all'
+              overflowWrap='anywhere'
+              textAlign='left'
+              px='0.5rem'
+            >
+              水中カメラマンのヒロキです！
+              <br />
+              いつも一眼レフで写真を撮っています！
+              <br />
+              <br />
+              広島県広島市出身の24歳。
+              <br />
+              広島の高専を卒業後、東京で電気メーカーのTV設計の仕事に3年弱従事していました。
+              2023年に退職をして趣味で始めた水中カメラを極める為に
+              宮古島への移住を決意しました。
+              <br />
+              海はもともと好きで高校２年生でスキューバダイビングのライセンスを取得するほどです。
+              <br />
+              僕たちのツアーで皆様の宮古島での素敵な思い出を残すお手伝いをさせてください。
+              <br />
+              全力でガイドをしていきます！
+              <br />
+              <br />
+              <span style={{ textDecoration: 'underline', fontWeight: 'bold' }}>
+                資格
+              </span>
+              <br />
+              ・NAUI スキンダイビングインストラクター
+              <br />
+              ・NAUI First Aider
+              <br />
+              ・NAUI Skindiving Rescue Specialty Diver
+              <br />
+              ・フリーダイビング AIDA2
+              <br />
+              ・PADI ADVANCED OPEN WATER DIVER
+            </Text>
+          </Box>
         </Flex>
 
         <Divider my='4rem' />
 
         <Flex
-          justify='space-evenly'
+          justify='space-between'
           align='flex-start'
-          flexDir={{ base: 'column', md: 'row' }}
-          mt='2rem'
+          flexDir={{ base: 'column', lg: 'row' }}
         >
-          <VStack width='68%'>
+          <Flex
+            justify='center'
+            align='center'
+            flexDir='column'
+            w={{ base: 'full', lg: '30%' }}
+          >
             <Image
-              src='about/staff_charlie.png'
-              borderRadius='full'
-              boxSize='200px'
+              src='about/staff_charlie.jpg'
+              borderRadius='50%'
+              boxSize='300px'
               alt='staff'
             />
             <Text fontSize='2xl' fontWeight='bold'>
               チャーリー
             </Text>
-          </VStack>
+          </Flex>
 
-          <Text
-            fontSize={{ base: 'lg', md: 'xl', lg: '2xl' }}
-            wordBreak='keep-all'
-            overflowWrap='anywhere'
-            textAlign='left'
-            px='0.5rem'
-          >
-            初めまして。チャーリーです。
-            <br />
-            <br />
-            シンガポール生まれ、奈良育ちの31歳。
-            <br />
-            大学を卒業後に大阪で化学品専門商社で営業の仕事を6年間従事していました。
-            <br />
-            2022年に退職し兼ねてより挑戦してみたかった自転車旅に出発。様々な縁が繋がり宮古島にたどり着きました。
-            <br />
-            現在は「Tabibito Cafe」という名称でCafeを経営しております。
-            <br />
-            僕は宮古島に来るまでは海とは無縁の生活を送っていましたが、宮古島の海の美しさに感動して、そのままこの地に移住。
-            <br />
-            この感動をたくさんの方に知ってもらいたく、たびんちゅを開業すること決意しました。
-            <br />
-            みなさまとお会いできることを楽しみにしています！
-            <br />
-            <br />
-            <span style={{ textDecoration: 'underline', fontWeight: 'bold' }}>
-              資格
-            </span>
-            <br />
-            ・NAUI スキンダイビングインストラクター
-            <br />
-            ・NAUI First Aider
-            <br />
-            ・NAUISkindiving Rescue Specialty Diver
-            <br />
-            ・フリーダイビング AIDA2,AIDA3
-          </Text>
+          <Box w={{ base: 'full', lg: '70%' }}>
+            <Text
+              fontSize={{ base: 'lg', md: 'xl', lg: '2xl' }}
+              wordBreak='keep-all'
+              overflowWrap='anywhere'
+              textAlign='left'
+              px='0.5rem'
+            >
+              初めまして。チャーリーです。
+              <br />
+              <br />
+              シンガポール生まれ、奈良育ちの31歳。
+              <br />
+              大学を卒業後に大阪で化学品専門商社で営業の仕事を6年間従事していました。
+              <br />
+              2022年に退職し兼ねてより挑戦してみたかった自転車旅に出発。様々な縁が繋がり宮古島にたどり着きました。
+              <br />
+              現在は「Tabibito Cafe」という名称でCafeを経営しております。
+              <br />
+              僕は宮古島に来るまでは海とは無縁の生活を送っていましたが、宮古島の海の美しさに感動して、そのままこの地に移住。
+              <br />
+              この感動をたくさんの方に知ってもらいたく、たびんちゅを開業すること決意しました。
+              <br />
+              みなさまとお会いできることを楽しみにしています！
+              <br />
+              <br />
+              <span style={{ textDecoration: 'underline', fontWeight: 'bold' }}>
+                資格
+              </span>
+              <br />
+              ・NAUI スキンダイビングインストラクター
+              <br />
+              ・NAUI First Aider
+              <br />
+              ・NAUI Skindiving Rescue Specialty Diver
+              <br />
+              ・フリーダイビング AIDA2,AIDA3
+            </Text>
+          </Box>
         </Flex>
       </Box>
     </>
